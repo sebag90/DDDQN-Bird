b719af8e4597991c5c5179eb681a3899f0652aff
seba <seba.g90@gmail.com>
2022-07-25 12:48:17 +0200

    check if args are present

--- dddqn-flappy.py
+++ dddqn-flappy.py
@@ -5,9 +5,10 @@ from utils.cli import get_arguments
 
 def main():
     args = get_arguments()
-    subparser = args.subparser
-    module = importlib.import_module(f"bin.{subparser}")
-    module.main(args)
+    if args:
+        subparser = args.subparser
+        module = importlib.import_module(f"bin.{subparser}")
+        module.main(args)
 
 
 if __name__ == "__main__":
